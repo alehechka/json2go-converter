1521513052a9924f28c38b91dd3610127e4b83ba
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2022-07-01 11:26:54 -0500

    reuse type definition as partial

--- client/src/api/generateTypes.ts
+++ client/src/api/generateTypes.ts
@@ -1,12 +1,9 @@
 import sanitizeMap from '../utils/sanitizeMap';
 import { ErrorResponse } from './errors';
+import { Defaults } from './getDefaults';
 import status from './status';
 
-export type GenerateTypesConfig = {
-	packageName?: string;
-	root?: string;
-	outputFile?: string;
-};
+export type GenerateTypesConfig = Partial<Defaults>;
 
 const generateTypes = (data: any, config?: GenerateTypesConfig): Promise<string | ErrorResponse> =>
 	fetch('/api/generate?' + new URLSearchParams(sanitizeMap<string>(config)), {
